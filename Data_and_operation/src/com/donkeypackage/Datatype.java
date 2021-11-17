008ddcda9bb26ad1b856abccbff7b1efc5f60beb
GoodDonkey <president20500@gmail.com>
2021-11-17 15:09:26 +0900

    initial commit

---
+++ Data_and_operation/src/com/donkeypackage/Datatype.java
@@ -0,0 +1,15 @@
+package com.donkeypackage;
+
+public class Datatype {
+
+    public static void main(String[] args) {
+        System.out.println(6); // Number
+        System.out.println("six"); // String
+        System.out.println("6"); // String 6
+        System.out.println(6+6); // 12
+
+        System.out.println("6"+"6"); // 66 -> 결합
+        System.out.println("1111".length()); // 문자열은 길이를 알 수 있다.
+
+    }
+}

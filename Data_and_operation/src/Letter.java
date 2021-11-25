ccabac11d1788b4d005ea189e1b6881dd5242826
GoodDonkey <president20500@gmail.com>
2021-11-25 19:55:23 +0900

    Variable

---
+++ Data_and_operation/src/Letter.java
@@ -0,0 +1,10 @@
+public class Letter {
+    public static void main(String[] args) {
+        String name = "donkey";
+        System.out.println("Hello, "+name+"...."+name+"....bye");  // 변수를 통해 반복적으로 적는 수고를 덜 수 있다.
+
+        double VAT = 10.0;
+        System.out.println(VAT); // 변수명이 VAT인 것을 보고 10.0 이라는 숫자가 부가가치세를 뜻하는 거라는 걸 알게 된다.
+        // 즉, 변수를 정의하는 것은 코드를 보고 의미를 파악하는 데에 도움을 준다.
+    }
+}
---
+++ Data_and_operation/src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1;  // Number -> Integer
+        System.out.println(a);
+
+        double b = 1.1; // real number -> double
+        System.out.println(b);
+
+        String c = "Hello World";
+        System.out.println(c);
+
+        float d = 1.1F;
+        System.out.println(d);
+    }
+}

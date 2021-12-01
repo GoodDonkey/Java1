a69458caf33e0ebdb33af671571597def52e055a
GoodDonkey <president20500@gmail.com>
2021-12-01 20:08:34 +0900

    Java Standard Library: Math

---
+++ Programming/src/ClassApp.java
@@ -0,0 +1,11 @@
+public class ClassApp {
+    public static void main(String[] args) {
+
+        System.out.println(Math.PI);
+        // 버림
+        System.out.println(Math.floor(1.6));
+        // 올림
+        System.out.println(Math.ceil(1.6));
+
+    }
+}

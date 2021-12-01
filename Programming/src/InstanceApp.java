8807005fff49334729bc20c381d7aae996b43558
GoodDonkey <president20500@gmail.com>
2021-12-01 20:28:29 +0900

    Java API: Inheritance

--- Programming/src/InstanceApp.java
+++ Programming/src/InstanceApp.java
@@ -13,6 +13,10 @@ public class InstanceApp {
         p2.write("Hello 2");
         p2.close();
 
+        // toString() 메서드 사용하기
+        // toString() 은 Object 클래스에 정의되어있으므로 사용할 수 있다.
+        System.out.println(p1.toString());
+
         // 아래와 같이 파일을 만들 때 마다 클래스마다 같은 클래스를 여러번 써야한다. 따라서 객체로 사용하는 것이 좋다.
         // PrintWriter.write("result1.txt", "Hello 1");
         // PrintWriter.write("result2.txt", "Hello 2");

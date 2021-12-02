279a5c02d7ceabe216d7f15491712f6d36e23bbb
GoodDonkey <president20500@gmail.com>
2021-12-02 15:51:29 +0900

    MyApp: variables

--- MyApp/src/AccountingApp.java
+++ MyApp/src/AccountingApp.java
@@ -1,18 +1,25 @@
 public class AccountingApp {
     public static void main(String[] args) {
+        double valueOfSupply = 10000.0;
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double vat = valueOfSupply * vatRate;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - expense;
+
         // 공급가
-        System.out.println("Value of supply : " + 10000.0);
+        System.out.println("Value of supply : " + valueOfSupply);
         // 부가가치세
-        System.out.println("VAT : " + (10000.0 * 0.1));
+        System.out.println("VAT : " + vat);
         // total
-        System.out.println("Total : " + 10000.0 + (10000.0 * 0.1));
+        System.out.println("Total : " + valueOfSupply + vat);
         // 비용
-        System.out.println("Expense : " + (10000.0 * 0.3));
+        System.out.println("Expense : " + expense);
         // 이익 (공급가 - 비용)
-        System.out.println("Income : " + (10000.0 - 10000.0 * 0.3));
+        System.out.println("Income : " + income);
         // 배당
-        System.out.println("Dividend : " + (10000.0 - 10000.0 * 0.3) * 0.5);
-        System.out.println("Dividend : " + (10000.0 - 10000.0 * 0.3) * 0.3);
-        System.out.println("Dividend : " + (10000.0 - 10000.0 * 0.3) * 0.2);
+        System.out.println("Dividend : " + income * 0.5);
+        System.out.println("Dividend : " + income * 0.3);
+        System.out.println("Dividend : " + income * 0.2);
     }
 }

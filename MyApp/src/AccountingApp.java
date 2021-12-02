0b2ea80e56e12f7e46fd8beb0d6e5333261760aa
GoodDonkey <president20500@gmail.com>
2021-12-02 15:51:29 +0900

    MyApp: using arguments

--- MyApp/src/AccountingApp.java
+++ MyApp/src/AccountingApp.java
@@ -1,11 +1,14 @@
 public class AccountingApp {
     public static void main(String[] args) {
-        double valueOfSupply = 10000.0;
+        double valueOfSupply = Double.parseDouble(args[0]);
         double vatRate = 0.1;
         double expenseRate = 0.3;
         double vat = valueOfSupply * vatRate;
         double expense = valueOfSupply * expenseRate;
         double income = valueOfSupply - expense;
+        double dividend1 = income * 0.5;
+        double dividend2 = income * 0.3;
+        double dividend3 = income * 0.2;
 
         // 공급가
         System.out.println("Value of supply : " + valueOfSupply);
@@ -18,8 +21,8 @@ public class AccountingApp {
         // 이익 (공급가 - 비용)
         System.out.println("Income : " + income);
         // 배당
-        System.out.println("Dividend : " + income * 0.5);
-        System.out.println("Dividend : " + income * 0.3);
-        System.out.println("Dividend : " + income * 0.2);
+        System.out.println("Dividend 1 : " + dividend1);
+        System.out.println("Dividend 2 : " + dividend2);
+        System.out.println("Dividend 3 : " + dividend3);
     }
 }

4fa1b800e776aebcf4cb3d121e03e3452bd34f77
GoodDonkey <president20500@gmail.com>
2021-12-02 15:51:29 +0900

    MyApp: instance

--- MyApp/src/AccountingMethodApp.java
+++ MyApp/src/AccountingMethodApp.java
@@ -1,23 +1,9 @@
-public class AccountingMethodApp {
-    private static double valueOfSupply;
-    private static double vatRate;
-    private static double expenseRate;
+class Accounting{
+    public double valueOfSupply;
+    public double vatRate;
+    public double expenseRate;
 
-    public static void main(String[] args) {
-        valueOfSupply = 10000;
-        vatRate = 0.1;
-        expenseRate = 0.3;
-
-        // 배당 비율을 array 에 담는다.
-        double[] dividendRates = new double[3];
-        dividendRates[0] = 0.5;
-        dividendRates[1] = 0.3;
-        dividendRates[2] = 0.2;
-
-        print(dividendRates);
-    }
-
-    private static void print(double[] dividendRates) {
+    public void print(double[] dividendRates) {
         // 공급가
         System.out.println("Value of supply : " + valueOfSupply);
         // 부가가치세
@@ -34,23 +20,48 @@ public class AccountingMethodApp {
         System.out.println("Dividend 3 : " + getDividend(dividendRates[2]));
     }
 
-    private static double getDividend(double dividendRate) {
+    private double getDividend(double dividendRate) {
         return getIncome() * dividendRate;
     }
 
-    private static double getIncome() {
+    private double getIncome() {
         return valueOfSupply - getExpense();
     }
 
-    private static double getExpense() {
+    private double getExpense() {
         return valueOfSupply * expenseRate;
     }
 
-    private static double getTotal() {
+    private double getTotal() {
         return valueOfSupply + getVat();
     }
 
-    private static double getVat() {
+    private double getVat() {
         return valueOfSupply * vatRate;
+    }
+}
+
+public class AccountingMethodApp {
+
+    public static void main(String[] args) {
+
+        // 배당 비율을 array 에 담는다.
+        double[] dividendRates = new double[3];
+        dividendRates[0] = 0.5;
+        dividendRates[1] = 0.3;
+        dividendRates[2] = 0.2;
+
+        // 인스턴스를 이용해 각각
+        Accounting a1 = new Accounting();
+        a1.valueOfSupply = 10000.0;
+        a1.vatRate = 0.1;
+        a1.expenseRate = 0.3;
+        a1.print(dividendRates);
+
+        Accounting a2 = new Accounting();
+        a2.valueOfSupply = 10000.0;
+        a2.vatRate = 0.05;
+        a2.expenseRate = 0.3;
+        a2.print(dividendRates);
     }
 }

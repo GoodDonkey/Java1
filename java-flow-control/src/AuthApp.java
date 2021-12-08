c78a9b240137167d564b4d1a753d98805ca61542
GoodDonkey <president20500@gmail.com>
2021-12-08 15:14:20 +0900

    conditional statement: using variable

--- java-flow-control/src/AuthApp.java
+++ java-flow-control/src/AuthApp.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class AuthApp {
+public class AuthApp {
+    public static void main(String[] args) {
+        // donkey 라는 값을 주면 로그인 텍스트를 출력하는 프로그램
+        System.out.println(args[0]);
+
+        String id = "donkey";
+        String inputId = args[0];
+
+        System.out.println("Hi.");
+
+        if (inputId.equals(id)) {
+            System.out.println("Master!");
+        } else {
+            System.out.println("who are you?");
+        }
+    }
 }

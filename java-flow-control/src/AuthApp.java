cbc2a98f829ed154ba811ddce3eaa4ccf8478f6b
GoodDonkey <president20500@gmail.com>
2021-12-08 15:14:20 +0900

    conditional statement: using variable(2)

--- java-flow-control/src/AuthApp.java
+++ java-flow-control/src/AuthApp.java
@@ -1,15 +1,27 @@
 public class AuthApp {
     public static void main(String[] args) {
-        // donkey 라는 값을 주면 로그인 텍스트를 출력하는 프로그램
+        // id와 pass 값을 주면 로그인 텍스트를 출력하는 프로그램
         System.out.println(args[0]);
 
         String id = "donkey";
         String inputId = args[0];
 
+        String pass = "1111";
+        String inputPass = args[1];
+
         System.out.println("Hi.");
 
-        if (inputId.equals(id)) {
-            System.out.println("Master!");
+//        if (inputId.equals(id)) {
+//            if (inputPass.equals(pass)) {
+//                System.out.println("Master!");
+//            }
+//        } else {
+//            System.out.println("who are you?");
+//        }
+
+        // 두 조건을 모두 만족하는지 체크: &&
+        if (inputId.equals(id) && inputPass.equals(pass)) {
+                System.out.println("Master!");
         } else {
             System.out.println("who are you?");
         }

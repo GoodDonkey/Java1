26043b63580b3b75d85b0a173b4f24a3ad9d56b7
GoodDonkey <president20500@gmail.com>
2021-12-08 14:47:41 +0900

    boolean type

---
+++ java-flow-control/src/BooleanApp.java
@@ -0,0 +1,16 @@
+public class BooleanApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("One");
+        System.out.println(1);
+        System.out.println(true);
+        System.out.println(false);
+
+        String foo = "Hello world";
+//        String true = "Hello world"; // reserved word
+
+        System.out.println(foo.contains("world "));  // true
+        System.out.println(foo.contains("donkey"));  // false
+    }
+}

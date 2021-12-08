4b8317caf170538981370cc66c4c9019efb39c51
GoodDonkey <president20500@gmail.com>
2021-12-08 15:14:20 +0900

    conditional statement

--- java-flow-control/src/IfApp.java
+++ java-flow-control/src/IfApp.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class IfApp {
+public class IfApp {
+    public static void main(String[] args) {
+        System.out.println("a");
+
+        if (false) {
+            System.out.println(1);
+        } else if (true){
+            System.out.println(2);
+        } else {
+            System.out.println(3);
+        }
+
+        System.out.println("b");
+    }
 }

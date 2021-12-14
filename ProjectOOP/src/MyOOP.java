4de5ee6beba33ff33a9a0342a1737c5eedbc34e0
GoodDonkey <president20500@gmail.com>
2021-12-14 18:47:57 +0900

    OOP: Instances

--- ProjectOOP/src/MyOOP.java
+++ ProjectOOP/src/MyOOP.java
@@ -1,11 +1,21 @@
 public class MyOOP {
     public static void main(String[] args) {
-        Print.delimiter = "----";
-        Print.printA();
-        Print.printA();
+        // 개별 인스턴스에 delimiter를 다르게 정의하여 일일이 값을 바꿔줄 필요가 없어졌다.
 
-        Print.delimiter = "****";
-        Print.printB();
-        Print.printB();
+        Print p1 = new Print();
+        p1.delimiter = "----";
+        p1.printA();
+        p1.printA();
+
+        Print p2 = new Print();
+        p2.delimiter = "****";
+        p2.printB();
+        p2.printB();
+
+
+        p2.printA();
+        p1.printA();
+        p1.printA();
+        p2.printA();
     }
 }
--- ProjectOOP/src/Print.java
+++ ProjectOOP/src/Print.java
@@ -1,13 +1,13 @@
 class Print {
-    public static String delimiter = "";
+    public String delimiter = "";
 
-    public static void printB() {
+    public void printB() {
         System.out.println(delimiter);
         System.out.println("B");
         System.out.println("B");
     }
 
-    public static void printA() {
+    public void printA() {
         System.out.println(delimiter);
         System.out.println("A");
         System.out.println("A");

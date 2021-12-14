de98cfc4b11e8ce04c9dffbee5453c4c771af68f
GoodDonkey <president20500@gmail.com>
2021-12-14 18:47:57 +0900

    OOP: Classes into seperate files

--- ProjectOOP/src/MyOOP.java
+++ ProjectOOP/src/MyOOP.java
@@ -9,19 +9,3 @@ public class MyOOP {
         Print.printB();
     }
 }
-
-class Print {
-    public static String delimiter = "";
-
-    public static void printB() {
-        System.out.println(delimiter);
-        System.out.println("B");
-        System.out.println("B");
-    }
-
-    public static void printA() {
-        System.out.println(delimiter);
-        System.out.println("A");
-        System.out.println("A");
-    }
-}
---
+++ ProjectOOP/src/Print.java
@@ -0,0 +1,15 @@
+class Print {
+    public static String delimiter = "";
+
+    public static void printB() {
+        System.out.println(delimiter);
+        System.out.println("B");
+        System.out.println("B");
+    }
+
+    public static void printA() {
+        System.out.println(delimiter);
+        System.out.println("A");
+        System.out.println("A");
+    }
+}

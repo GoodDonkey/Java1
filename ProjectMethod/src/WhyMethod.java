a7cbf96d84be58fda8d2903b5cc55c29ad91ba3a
GoodDonkey <president20500@gmail.com>
2021-12-14 15:23:25 +0900

    Method

---
+++ ProjectMethod/src/WhyMethod.java
@@ -0,0 +1,13 @@
+public class WhyMethod {
+    public static void main(String[] args) {
+        printTwoTimesA();
+        printTwoTimesA();
+        printTwoTimesA();
+    }
+
+    private static void printTwoTimesA() {
+        System.out.println("-");
+        System.out.println("B");
+        System.out.println("B");
+    }
+}

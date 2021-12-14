9f4984c5e2f21e2fe8ae95eb05c4c20b2bfe76e7
GoodDonkey <president20500@gmail.com>
2021-12-14 18:47:57 +0900

    OOP: Accounting app instanciation

--- ProjectOOP/src/AccountingApp.java
+++ ProjectOOP/src/AccountingApp.java
@@ -1,12 +1,16 @@
 class Accounting {
-    public static double valueOfSupply;
+    public double valueOfSupply;
     public static double vatRate = 0.1;
 
-    public static double getTotal() {
+    public Accounting(double valueOfSupply) {
+        this.valueOfSupply = valueOfSupply;
+    }
+
+    public double getTotal() {
         return valueOfSupply + getVAT();
     }
 
-    public static double getVAT() {
+    public double getVAT() {
         return valueOfSupply * vatRate;
     }
 }
@@ -14,10 +18,11 @@ class Accounting {
 public class AccountingApp {
 
     public static void main(String[] args) {
-        Accounting.valueOfSupply = 10000.0;
-        System.out.println(Accounting.valueOfSupply);
-        System.out.println(Accounting.getVAT());
-        System.out.println(Accounting.getTotal());
+        Accounting a1 = new Accounting(10000.0);
+        System.out.println(a1.valueOfSupply);
+
+        Accounting a2 = new Accounting(20000.0);
+        System.out.println(a2.valueOfSupply);
     }
 
 

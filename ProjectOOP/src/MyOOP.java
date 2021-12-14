f227248a718dd7ed6c205a4a584b5b8ffd6e49d4
GoodDonkey <president20500@gmail.com>
2021-12-14 18:47:57 +0900

    OOP: Constructor and this

--- ProjectOOP/src/MyOOP.java
+++ ProjectOOP/src/MyOOP.java
@@ -2,13 +2,11 @@ public class MyOOP {
     public static void main(String[] args) {
         // 개별 인스턴스에 delimiter를 다르게 정의하여 일일이 값을 바꿔줄 필요가 없어졌다.
 
-        Print p1 = new Print();
-        p1.delimiter = "----";
+        Print p1 = new Print("----");
         p1.printA();
         p1.printA();
 
-        Print p2 = new Print();
-        p2.delimiter = "****";
+        Print p2 = new Print("****");
         p2.printB();
         p2.printB();
 
--- ProjectOOP/src/Print.java
+++ ProjectOOP/src/Print.java
@@ -1,6 +1,10 @@
 class Print {
     public String delimiter = "";
 
+    public Print(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
     public void printB() {
         System.out.println(delimiter);
         System.out.println("B");
